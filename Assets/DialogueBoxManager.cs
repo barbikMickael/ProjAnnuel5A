907b1ee72b3174ce635eb18c9944aa5d9cc11321
Cocquempot Mickael <m.cocquempot@gmail.com>
2016-05-30 16:44:38 +0200

    Mise en place du projet annuel avec Asset perso fée et perso principal, et une maison

---
+++ Assets/DialogueBoxManager.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class DialogueBoxManager : MonoBehaviour {
+
+    public GameObject textBox;
+    public Text theText;
+    public TextAsset textFile;
+    public string[] textLines;
+    public GestionEmotion playerEMotion;
+    public int currentLine;
+    public int endAtLine;
+    // Use this for initialization
+    void Start () {
+        if (textFile != null)
+        {
+            textLines = (textFile.text.Split('\n'));
+        }
+        if (endAtLine == 0)
+        {
+           endAtLine = textLines.Length - 1;
+        }
+        theText.text = textLines[currentLine];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            NextDialogue();
+        }
+            
+	}
+
+    void NextDialogue()
+    {
+        // arbres de dialogues
+        if (playerEMotion.GetEmotion()<8)
+        {
+            if (currentLine < endAtLine)
+            {
+                currentLine += 2;
+            }
+        }
+        else if (playerEMotion.GetEmotion() > 13)
+        {
+            if (currentLine < endAtLine)
+            {
+                currentLine += 1;
+            }
+        }
+
+
+        theText.text = textLines[currentLine];
+
+    }
+}
---
+++ Assets/GestionEmotion.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GestionEmotion : MonoBehaviour {
+    int emotion = 10;
+    public GameObject queryChan;
+    public Image jauge;
+    private Color myColor;
+    QuerySDEmotionalController qec;
+
+    /*
+    changer jauge et mettre chaque émotions avec une jauge et up les jauges correspondantes
+        */
+
+
+    // Use this for initialization
+    void Start () {
+        //qec = GetComponent<QuerySDEmotionalController>();
+        myColor = new Color(2.0f * 1/emotion * 5, 2.0f * (1 - 1/emotion * 5), 0);
+        jauge.color = myColor;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        //pulling d'émotion renvoyé par les lunettes
+        if (Input.GetKeyUp(KeyCode.Z))
+        {
+            if (emotion < 21)
+            {
+                emotion++;
+                myColor = new Color(2.0f * 1 / emotion * 5, 2.0f * (1 - 1 / emotion * 5), 0);
+                jauge.color = myColor;
+                Debug.Log("z" + " " + emotion);
+            }
+        }
+
+        else if (Input.GetKeyUp(KeyCode.S))
+        {
+            if (emotion > 1)
+            {
+                emotion--;
+                myColor = new Color(2.0f * 1 / emotion * 5, 2.0f * (1 - 1 / emotion * 5), 0);
+                jauge.color = myColor;
+                Debug.Log("s" + " " + emotion);
+            }
+        }
+       
+        if (emotion < 8)
+            queryChan.GetComponent<QuerySDEmotionalController>().ChangeEmotion(QuerySDEmotionalController.QueryChanSDEmotionalType.NORMAL_ANGER);
+        else if (emotion > 14)
+            queryChan.GetComponent<QuerySDEmotionalController>().ChangeEmotion(QuerySDEmotionalController.QueryChanSDEmotionalType.NORMAL_SMILE);
+        else
+            queryChan.GetComponent<QuerySDEmotionalController>().ChangeEmotion(QuerySDEmotionalController.QueryChanSDEmotionalType.NORMAL_DEFAULT);
+
+    }
+    public int GetEmotion()
+    {
+        return emotion;
+    }
+}
