a9079c172628ed627390b3e55d9fd60cb2605f51
Benoit Olivier <benoit.oliviermuzard@gmail.com>
2016-06-01 12:11:38 +0200

    Ocean Scene, Assets moved, scripts disable objects.

---
+++ Assets/Custom/Scripts/ActivateObjectsOnTimer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateObjectsOnTimer : MonoBehaviour {
+
+    public GameObject[] objectsToDisable;
+    public GameObject[] objectsToEnable;
+    public float timesToDisable;
+    public float timesToEnable;
+
+    void Start()
+    {
+        StartCoroutine(TimerToActivate());
+        StartCoroutine(TimerToDisActivate());
+    }
+    IEnumerator TimerToDisActivate()
+    {
+        yield return new WaitForSeconds(timesToDisable);
+        for (int i = 0; i < objectsToDisable.Length; i++)
+        {
+            objectsToDisable[i].SetActive(false);
+        }
+    }
+    IEnumerator TimerToActivate()
+    {
+        yield return new WaitForSeconds(timesToEnable);
+        for (int i = 0; i < objectsToEnable.Length; i++)
+        {
+            objectsToEnable[i].SetActive(true);
+        }
+    }
+}
---
+++ Assets/Custom/Scripts/AnimationEnd.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEnd : MonoBehaviour {
+    public SceneManager myManager;
+    public int number; 
+	// Use this for initialization
+	void OnEnable () {
+        myManager.OnAnimationFinished(number);
+	}
+}
