290173d24fd2a2332a85f88af116369acf6deac7
Benoit Olivier <benoit.oliviermuzard@gmail.com>
2016-06-01 17:34:55 +0200

    Standard Asset + Low Poly ville

--- Assets/Custom/Scripts/AnimationEnd.cs
+++ Assets/Custom/Scripts/AnimationEnd.cs
@@ -3,9 +3,8 @@ using System.Collections;
 
 public class AnimationEnd : MonoBehaviour {
     public SceneManager myManager;
-    public int number; 
 	// Use this for initialization
 	void OnEnable () {
-        myManager.OnAnimationFinished(number);
-	}
+        myManager.OnAnimationFinished();
+    }
 }
---
+++ Assets/Custom/Scripts/SceneControllers/SceneManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+    public Camera[] cameraArray;
+    public Animation[] animationsArray;
+    protected int indexCamera = 0;
+    // Use this for initialization
+    void OnEnable () {
+        for (int i = 0; i < cameraArray.Length; i++)
+        {
+            cameraArray[i].enabled = false;
+        }
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnAnimationFinished()
+    {
+        cameraArray[indexCamera].enabled = false;
+        indexCamera++;
+        cameraArray[indexCamera].enabled = true;
+
+    }
+
+    public int getCameraIndex()
+    {
+        return indexCamera;
+    }
+    public void setCameraIndex(int i)
+    {
+        indexCamera = i;
+    }
+}
