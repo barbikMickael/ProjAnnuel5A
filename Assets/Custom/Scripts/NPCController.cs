dd1728f267c993df39eb3e45ea717b44d2ab6a27
Benoit Olivier <benoit.oliviermuzard@gmail.com>
2016-06-02 10:57:30 +0200

    OceanTest 1ere integration Chat Manager

---
+++ Assets/Custom/Scripts/NPCController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField]
+    protected GameObject queryChan;
+    //===============================
+    [SerializeField]
+    QuerySDMecanimController.QueryChanSDAnimationType defaultAnimType = QuerySDMecanimController.QueryChanSDAnimationType.NORMAL_STAND;
+    [SerializeField]
+    QuerySDEmotionalController.QueryChanSDEmotionalType defaultFaceType = QuerySDEmotionalController.QueryChanSDEmotionalType.NORMAL_DEFAULT;
+    // Use this for initialization
+
+    public float newAnimType;
+    private int lastAnimType;
+
+    void OnEnable()
+    {
+        newAnimType = (int)defaultAnimType;
+        if (queryChan == null)
+        {
+            queryChan = this.gameObject;
+        }
+    }
+
+    void Start () {
+        ChangeAnimation((int)defaultAnimType);
+        ChangeEmotion(defaultFaceType);
+        lastAnimType = (int)defaultAnimType;
+    }
+
+    void Update()
+    {
+        if ((int)newAnimType != lastAnimType)
+        {
+            lastAnimType = (int) newAnimType;
+            ChangeAnimation(lastAnimType);
+        }
+    }
+    public void ChangeEmotion(QuerySDEmotionalController.QueryChanSDEmotionalType faceNumber)
+    {
+        queryChan.GetComponent<QuerySDEmotionalController>().ChangeEmotion(faceNumber);
+    }
+
+    public void ChangeAnimation(int animNumber)
+    {
+        queryChan.GetComponent<QuerySDMecanimController>().ChangeAnimation((QuerySDMecanimController.QueryChanSDAnimationType)animNumber);
+    }
+}
